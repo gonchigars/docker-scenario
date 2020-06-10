b6dbfac7a84d2da2e31971c6be97baa3937e966f
Dhruv Bhartia <dhruvbhartia425@gmail.com>
2020-06-10 16:38:49 +0530

    Update step1-verify.sh

--- demo-scenario/step1-verify.sh
+++ demo-scenario/step1-verify.sh
@@ -1,7 +1,11 @@
 $op=$(docker ps | grep ubuntu)
+echo "op = $op"
 if [ "$op" == "" ]; then
+  echo "Container is stop"
+  echo "Checking stopped containers"
   $op=$(docker ps -a | grep ubuntu)
   if [ "$op" != "" ]; then
+    echo "Container exists"
     echo "done"
   fi
 fi
