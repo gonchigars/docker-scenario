c90fa51010c3f664ecf2afda4108204bd0088136
Dhruv Bhartia <dhruvbhartia425@gmail.com>
2020-06-15 17:04:40 +0530

    Update step1-verify.sh

--- k8s-basic/step1-verify.sh
+++ k8s-basic/step1-verify.sh
@@ -6,7 +6,7 @@ name=$(kubectl describe deployment hello-world | grep Name: | awk '{print $2}')
 labels=$(kubectl describe deployment hello-world | grep Labels: | awk 'NR==1{print $2}')
 img=$(kubectl describe deployment hello-world | grep Image: | awk '{print $2}')
 op=$(kubectl get deployments --show-labels | grep "$name" | grep "$labels" | awk '{print $2}')
-if [ "$op" == "3/3" && "$name" == "$actName" && "$labels" == "$actLabels" && "$img" == "$actImg"]
+if [ "$op" == "3/3" ] && [ "$name" == "$actName" ] && [ "$labels" == "$actLabels" ] && [ "$img" == "$actImg" ]
 then
   echo "done"
 fi
